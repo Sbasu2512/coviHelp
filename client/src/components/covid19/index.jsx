efcdbd9c6e3210039e5727595ad67db367eb1fd8
Ali Salim <alisalim.ca@gmail.com>
2021-08-05 18:42:37 -0400

    updated import route

--- client/src/components/covid19/index.jsx
+++ client/src/components/covid19/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import TestingLocations from "../testing-locations/index"
+import TestingLocations from "./testing-locations/index"
 
 
 const Covid = () => {
