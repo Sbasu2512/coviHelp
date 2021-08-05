4d252abcf021aa6547b331d42fd631fb3cf8c9a4
Ali Salim <alisalim.ca@gmail.com>
2021-08-05 00:39:55 +0000

    added a backup server rails -v 5.2.6

---
+++ covid_help_server/app/controllers/api/testing_locations_controller.rb
@@ -0,0 +1,2 @@
+class Api::TestingLocationsController < ApplicationController
+end
---
+++ covid_help_server/test/controllers/api/testing_locations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::TestingLocationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
